82b6f068966dd17eb7ab0ae7bc80e400558e9db2
Gvidas <gvisis@gmail.com>
2021-05-14 17:41:02 +0300

    initial commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Login = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isError, setIsError] = useState(false)
+    // const [user, setUser] = useState();
+    
+    return (
+        <>
+        </>
+    )
+}
+
+export default Login;
+
+
