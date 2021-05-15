ce22114ed42913f8e45c6df8ea8c721078d6cfec
Gvidas <gvisis@gmail.com>
2021-05-15 23:34:38 +0300

    chat box styling update

--- src/components/ChatApp/ChatWindow/ChatWindow.js
+++ src/components/ChatApp/ChatWindow/ChatWindow.js
@@ -11,7 +11,6 @@ function ChatWindow() {
         </div>
       </header>
       <main className="chat_container-messages padding-10">
-
         <div className="message other_message">
           <img
             src="https://randomuser.me/api/portraits/thumb/women/17.jpg"
@@ -23,22 +22,34 @@ function ChatWindow() {
             odit atque tempore quo quidem, doloribus dolor rerum dolores odio
             minus placeat debitis dicta voluptas animi adipisci id! Veritatis,
             delectus dolore!
-          <div className="message-time">05-15 14:46 PM</div>
+          <div className="message-time">14:46 PM</div>
           </div>
         </div>
+
         <div className="message user_message">
           <img
             src="https://randomuser.me/api/portraits/thumb/men/17.jpg"
             alt=""
-            className='img_user'
+            className="img_user"
           />
           <div className="message-text">
-            Lorem ipsum dolor sit am
-            delectus dolore!
-          <div className="message-time">05-15 14:46 PM</div>
+            Lorem ipsum dolor sit am delectus dolore!
+            <div className="message-time">14:46 PM</div>
           </div>
         </div>
-
+        <div className="message other_message">
+          <img
+            src="https://randomuser.me/api/portraits/thumb/women/17.jpg"
+            alt=""
+            className="img_other"
+          />
+          <div className="message-text">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates
+            odit atque tempore quo quid placeat debitis dicta voluptas animi adipisci id! Veritatis,
+            delectus dolore!
+          <div className="message-time">14:46 PM</div>
+          </div>
+        </div>
       </main>
       <footer className="chat_container-send padding-10">footer</footer>
     </section>
